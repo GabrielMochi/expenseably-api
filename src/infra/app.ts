990ceeae0cb14ba9a01677273e173723229b27a8
Gabriel Mochi <gmochi56@icloud.com>
2022-04-04 04:59:43 -0300

    adds loggin middleware to requests

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import pino from "express-pino-logger";
+import { logger } from "./logger";
 
 export const app = express();
+
+app.use(pino({ logger }));
 
 app.get("/", (req, res) => {
   res.status(200).send("OK");
