2bcd12e4a4420728dc755981c5423812130e1844
Gabriel Mochi <gmochi56@icloud.com>
2022-04-04 14:09:28 -0300

    removes password from redis connection

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,4 +1,2 @@
 export const REDIS_HOST = process.env.REDIS_HOST;
 export const REDIS_PORT = process.env.REDIS_PORT;
-export const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
-export const REDIS_URI = `redis://:${encodeURI(REDIS_PASSWORD)}@${REDIS_HOST}:${REDIS_PORT}`;
--- src/infra/redis.ts
+++ src/infra/redis.ts
@@ -1,11 +1,10 @@
-import { REDIS_HOST, REDIS_PASSWORD, REDIS_PORT } from "@config/redis.config";
+import { REDIS_HOST, REDIS_PORT } from "@config/redis.config";
 import { createClient } from "redis";
 import { logger } from "./logger";
 
 export const redisClient = createClient({
   host: REDIS_HOST,
   port: Number(REDIS_PORT),
-  password: REDIS_PASSWORD,
 });
 
 redisClient.on("ready", () => {
