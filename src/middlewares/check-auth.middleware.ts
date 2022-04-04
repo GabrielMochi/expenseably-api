04d5e6b127e8e2427fc2fe2e7f4ea225cb8346d4
Gabriel Mochi <gmochi56@icloud.com>
2022-04-04 18:55:19 -0300

    fixes wrong path to auth

--- src/middlewares/check-auth.middleware.ts
+++ src/middlewares/check-auth.middleware.ts
@@ -1,7 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
 import { unauthorized } from "boom";
 
-const allowedPaths = ["/auth", "/health"];
+const allowedPaths = ["/api/v1/auth", "/health"];
 
 export const checkAuth = (req: Request, res: Response, next: NextFunction): void => {
   if (allowedPaths.includes(req.path) || !!req.session.auth) return next();
