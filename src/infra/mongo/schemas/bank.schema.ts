2c8a20352fd12084242a3cfa397eb23303482101
Gabriel Mochi <gmochi56@icloud.com>
2022-04-05 03:20:15 -0300

    sets createdAt timestamp

--- src/infra/mongo/schemas/bank.schema.ts
+++ src/infra/mongo/schemas/bank.schema.ts
@@ -1,10 +1,15 @@
 import { Bank } from "@domain/bank.domain";
 import { Schema } from "mongoose";
 
-export const bankSchema = new Schema<Bank>({
-  name: { type: String, required: true },
-  user: { type: Schema.Types.ObjectId, ref: "User" },
-});
+export const bankSchema = new Schema<Bank>(
+  {
+    name: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  {
+    timestamps: { createdAt: true, updatedAt: false },
+  },
+);
 
 bankSchema.virtual("id").get(function () {
   return this._id.toHexString();
