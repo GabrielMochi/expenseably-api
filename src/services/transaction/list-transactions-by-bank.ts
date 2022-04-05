d08036051395c9298b4fddd06adb1c830bed588a
Gabriel Mochi <gmochi56@icloud.com>
2022-04-05 07:48:59 -0300

    fixes list transaction by bank

--- src/services/transaction/list-transactions-by-bank.ts
+++ src/services/transaction/list-transactions-by-bank.ts
@@ -1,19 +1,29 @@
 import { LoadQueryParams, Transaction } from "@domain/transaction.domain";
 import { TransactionModel } from "@models/transaction.model";
-import { Types } from "mongoose";
+import { Types, FilterQuery } from "mongoose";
 
 export const listTransactionsByBank = async (
   bankId: string,
   queryParams?: LoadQueryParams,
 ): Promise<Transaction[]> => {
-  const transactions = await TransactionModel.find(
-    {
-      bank: new Types.ObjectId(bankId),
-      category: queryParams?.category,
-      $text: { $search: queryParams?.search },
-    },
-    { score: { $meta: "textScore" } },
-  ).sort({ score: { $meta: "textScore" } });
+  const filter: FilterQuery<Transaction> = {
+    bank: new Types.ObjectId(bankId),
+  };
+
+  const projection: Record<string, unknown> = {};
+
+  if (queryParams?.category) filter.category = queryParams?.category;
+
+  if (queryParams?.search) {
+    filter.$text = { $search: queryParams?.search };
+    projection.score = { $meta: "textScore" };
+  }
+
+  const sort = queryParams?.search
+    ? { score: { $meta: "textScore" } }
+    : { createdAt: "descending" };
+
+  const transactions = await TransactionModel.find(filter, projection).sort(sort);
 
   for (const transaction of transactions) {
     await transaction.populate("bank");
