6b8a1ffcd3b9f3f5c4a1e02e745ff714a7e9668f
Gabriel Mochi <gmochi56@icloud.com>
2022-04-04 21:46:16 -0300

    generates md5 hashes from passwords

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,10 @@
 import { Authentication } from "@domain/authentication.domain";
 import { isAuthenticationValid } from "@services/is-authentication-valid";
+import md5 from "md5";
 
 export class AuthController {
-  public async isAuthenticationValid(authentication: Authentication): Promise<boolean> {
-    const isValid = await isAuthenticationValid(authentication);
+  public async isAuthenticationValid({ username, password }: Authentication): Promise<boolean> {
+    const isValid = await isAuthenticationValid({ username, password: md5(password) });
     return isValid;
   }
 }
