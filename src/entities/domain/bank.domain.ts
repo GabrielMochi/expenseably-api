a853d01ca916fc179306d3cc4ff2d715815abba3
Gabriel Mochi <gmochi56@icloud.com>
2022-04-05 00:42:49 -0300

    adds validation decorators and creates dto

--- src/entities/domain/bank.domain.ts
+++ src/entities/domain/bank.domain.ts
@@ -1,8 +1,39 @@
+import { Expose, Type } from "class-transformer";
+import {
+  IsDate,
+  IsDefined,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  IsString,
+  ValidateNested,
+} from "class-validator";
 import { User } from "./user.domain";
 
 export class Bank {
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  @Expose()
   id: string;
+
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  @Expose()
   name: string;
+
+  @IsDefined()
+  @IsObject()
+  @IsNotEmptyObject()
+  @ValidateNested()
+  @Type(() => User)
+  @Expose()
   user: User;
+
+  @IsDefined()
+  @IsDate()
+  @IsNotEmpty()
+  @Expose()
   createdAt: Date;
 }
--- src/entities/domain/user.domain.ts
+++ src/entities/domain/user.domain.ts
@@ -1,6 +1,28 @@
+import { Expose } from "class-transformer";
+import { IsDate, IsDefined, IsNotEmpty, IsString } from "class-validator";
+
 export class User {
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  @Expose()
   id: string;
+
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  @Expose()
   name: string;
+
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  @Expose()
   email: string;
+
+  @IsDefined()
+  @IsDate()
+  @IsNotEmpty()
+  @Expose()
   createdAt: Date;
 }
