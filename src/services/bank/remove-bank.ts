f10c542bccb750b8360fe1e89d57dc53889c8139
Gabriel Mochi <gmochi56@icloud.com>
2022-04-05 02:44:39 -0300

    updates remove bank implementatin

--- src/services/bank/remove-bank.ts
+++ src/services/bank/remove-bank.ts
@@ -1,5 +1,8 @@
+import { NotFoundException } from "@exceptions/not-found.exception";
 import { BankModel } from "@models/bank.model";
 
 export const removeBank = async (id: string): Promise<void> => {
-  await BankModel.findByIdAndRemove(id);
+  const bank = await BankModel.findById(id);
+  if (!bank) throw new NotFoundException();
+  await bank.remove();
 };
